067d3a536d9c21201c55d6306f17251cfd7adc48
Zack Hanni <zack.hannI@gmail.com>
2024-03-08 13:14:06 -0500

    Create InputForm.tsx

---
+++ app/components/InputForm.tsx
@@ -0,0 +1,56 @@
+interface InputFormProps {
+  setStartingCapital: React.Dispatch<React.SetStateAction<number>>;
+  setMonthlyInvestment: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function InputForm({
+  setStartingCapital,
+  setMonthlyInvestment,
+}: InputFormProps) {
+  return (
+    <div className="bg-gray-200 flex items-center justify-center py-8">
+      <form
+        // action="/action_page.php"
+        // onSubmit={}
+        className="flex flex-col max-w-screen-sm space-y-4"
+      >
+        <div className="flex flex-col">
+          <label htmlFor="initial-capital">Starting capital</label>
+          <input
+            type="number"
+            id="initial-capital"
+            name="initial capital"
+            onChange={(e) => setStartingCapital(Number(e.currentTarget.value))}
+            placeholder="0"
+            className="px-2 py-2"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="investment">Monthly investment</label>
+          <input
+            type="number"
+            id="investment"
+            name="investment"
+            onChange={(e) =>
+              setMonthlyInvestment(Number(e.currentTarget.value))
+            }
+            placeholder="0"
+            className="px-2 py-2"
+          />
+        </div>
+
+        {/* <label htmlFor="country">Investment interval</label>
+        <select id="country" name="country">
+          <option value="australia">Weekly</option>
+          <option value="canada">Monthly</option>
+          <option value="usa">Yearly</option>
+        </select> */}
+
+        {/* <button type="submit" value="Submit" className="bg-green-500 py-2">
+          Submit
+        </button> */}
+      </form>
+    </div>
+  );
+}
