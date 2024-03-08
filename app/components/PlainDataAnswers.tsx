2136ac614161069883931f1378f639f5d8b95ecf
Zack Hanni <zack.hannI@gmail.com>
2024-03-08 16:20:50 -0500

    added apy calculation and calculateMonthsToReachGoal function

--- app/components/PlainDataAnswers.tsx
+++ app/components/PlainDataAnswers.tsx
@@ -2,37 +2,54 @@
 import React from "react";
 
 const goals = [
-  100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000,
-  1000000,
+  100_000, 200_000, 300_000, 400_000, 500_000, 600_000, 700_000, 800_000,
+  900_000,
 ];
 
 interface PlainDataAnswersProps {
   startingCapital: number;
   monthlyInvestment: number;
+  apy: number;
 }
 
 export default function PlainDataAnswers({
   startingCapital,
   monthlyInvestment,
+  apy,
 }: PlainDataAnswersProps) {
+  const calculateMonthsToReachGoal = (goal: number) => {
+    let months = 0;
+    let total = startingCapital;
+    const monthlyRate = apy / 12 / 100;
+
+    while (total < goal) {
+      total += monthlyInvestment;
+      total += total * monthlyRate;
+      months++;
+    }
+    return months;
+  };
+
   return (
     <div className="bg-gray-400 py-8">
       {/* <p>Starting capital: {startingCapital}</p> */}
       {/* <p>Monthly investment: {monthlyInvestment}</p> */}
       <div className="space-y-4 flex flex-col items-center justify-center ">
-        {startingCapital > 0 && monthlyInvestment > 0 ? (
+        {startingCapital > 0 && monthlyInvestment > 0 && apy > 0 ? (
           goals.map((goal) =>
             startingCapital > goal ? null : (
-              <div key={goal} className="bg-gray-200 px-4 py-2 flex space-x-8">
+              <div
+                key={goal}
+                className="bg-gray-200 px-4 py-2 flex space-x-8 items-center"
+              >
                 <p>Months until: {goal}</p>
-                <p className="bg-gray-300">
-                  {Math.floor((goal - startingCapital) / monthlyInvestment)}
+                <p className="bg-gray-300 p-1">
+                  {calculateMonthsToReachGoal(goal)}
                 </p>
                 <p>Months until next 100k</p>
-                <p className="bg-gray-300">
-                  {Math.floor(
-                    (goal + 100_000 - startingCapital) / monthlyInvestment
-                  ) - Math.floor((goal - startingCapital) / monthlyInvestment)}
+                <p className="bg-gray-300 p-1">
+                  {calculateMonthsToReachGoal(goal + 100_000) -
+                    calculateMonthsToReachGoal(goal)}
                 </p>
               </div>
             )
@@ -40,6 +57,16 @@ export default function PlainDataAnswers({
         ) : (
           <p>Enter data into the form above to get started</p>
         )}
+        <div className="bg-gray-200 py-4 px-8 rounded-lg">
+          <p>Want to be a millionaire?</p>
+          <p>At this rate, becoming a millionaire will take:</p>
+          <p>
+            {calculateMonthsToReachGoal(1_000_000) < 12
+              ? calculateMonthsToReachGoal(1_000_000) + " months"
+              : Math.floor(calculateMonthsToReachGoal(1_000_000) / 12) +
+                " years"}
+          </p>
+        </div>
       </div>
     </div>
   );
