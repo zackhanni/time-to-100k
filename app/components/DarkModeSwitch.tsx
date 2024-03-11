62d87c652d7ffc45194737d306566e7bd0420e72
Zack Hanni <zack.hannI@gmail.com>
2024-03-11 00:43:19 -0400

    Create DarkModeSwitch.tsx

---
+++ app/components/DarkModeSwitch.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
+interface DarkModeProps {
+  setDarkMode: (darkMode: boolean) => void;
+}
+
+export default function DarkModeSwitch({ setDarkMode }: DarkModeProps) {
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <div className="h-16 w-full flex items-center justify-between px-8 md:px-0 max-w-screen-md mx-auto">
+      <div>Time to 100k</div>
+      <button
+        className={`text-sm rounded-full px-2 py-1 hover:underline ${
+          clicked
+            ? "bg-white text-black"
+            : " bg-black text-white border-b-4 border-r-4"
+        }`}
+        onClick={() => {
+          setClicked(!clicked);
+          setDarkMode(!clicked);
+        }}
+      >
+        {clicked ? "Light Mode" : "Dark Mode"}
+      </button>
+    </div>
+  );
+}
