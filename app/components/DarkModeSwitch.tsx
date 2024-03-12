11a57760d8f5fdec3d7db7d8262a9dd888ee2d92
Zack Hanni <zack.hannI@gmail.com>
2024-03-12 01:08:15 -0400

    added button border to both light and dark mode button. make page title bold

--- app/components/DarkModeSwitch.tsx
+++ app/components/DarkModeSwitch.tsx
@@ -11,11 +11,11 @@ export default function DarkModeSwitch({ setDarkMode }: DarkModeProps) {
 
   return (
     <div className="h-16 w-full flex items-center justify-between px-8 md:px-0 max-w-screen-md mx-auto">
-      <div>Time to 100k</div>
+      <div className="font-bold">Time to 100k</div>
       <button
         className={`text-sm rounded-full px-2 py-1 hover:underline ${
           clicked
-            ? "bg-white text-black"
+            ? "bg-white text-black border-b-4 border-r-4 border-gray-400"
             : " bg-black text-white border-b-4 border-r-4"
         }`}
         onClick={() => {
