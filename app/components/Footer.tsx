379c7ae25b8a15de2e2552383d7fcafeb81663de
Zack Hanni <zack.hannI@gmail.com>
2024-03-12 00:57:16 -0400

    lowered header size in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
   return (
     <section className="py-16 text-white bg-gray-700 flex flex-col items-center space-y-8">
       <div className="flex items-center justify-center">
-        <h2 className="text-4xl max-w-xs">
+        <h2 className="text-2xl max-w-xs">
           Interested in learning more about personal finance?
         </h2>
         <div className="flex flex-col">
@@ -41,7 +41,7 @@ export default function Footer() {
           })}
         </div>
       </div>
-      <Link href="https://www.zachary.works/" className="hover:underline -p-8">
+      <Link href="https://www.zachary.works/" className="hover:underline ">
         Website created by Zack Hanni. Click to see more.
       </Link>
     </section>
